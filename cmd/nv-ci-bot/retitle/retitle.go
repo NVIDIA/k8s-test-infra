3d419989a6824426ffa3c7af68bfccdf21e95871
Carlos Eduardo Arango Gutierrez <eduardoa@nvidia.com>
2024-02-29 18:41:41 +0100

    QUick look
    Signed-off-by: Carlos Eduardo Arango Gutierrez <eduardoa@nvidia.com>

--- cmd/nv-ci-bot/retitle/retitle.go
+++ cmd/nv-ci-bot/retitle/retitle.go
@@ -17,9 +17,13 @@
 package retitle
 
 import (
+	"context"
+	"os"
 	"regexp"
 	"strings"
 
+	"golang.org/x/oauth2"
+	"gopkg.in/yaml.v2"
 	"k8s.io/klog/v2"
 
 	"github.com/google/go-github/v59/github"
@@ -64,16 +68,49 @@ func (m command) build() *cli.Command {
 func (m command) run(c *cli.Context) error {
 	action := githubactions.New()
 
+	// Create the GitHub client
+	accessToken := os.Getenv("INPUT_GITHUB-TOKEN")
+	ts := oauth2.StaticTokenSource(
+		&oauth2.Token{AccessToken: accessToken},
+	)
+	tc := oauth2.NewClient(c.Context, ts)
+	gh := github.NewClient(tc)
+
+	// Get the event
 	context, err := action.Context()
 	if err != nil {
 		m.log.Error(err, "Failed to get action context")
 		return err
 	}
+	org, repo := context.Repo()
 
-	var newTitle string
-	var issueNumber int
+	// Check if user is trusted first
+	var user string
 	if context.Event != nil {
 		if comment, ok := context.Event["comment"].(map[string]any); ok {
+			if userMap, ok := comment["user"].(map[string]any); ok {
+				if login, ok := userMap["login"].(string); ok {
+					user = login
+				}
+			}
+		}
+	}
+	trusted, err := isTrusted(c.Context, gh, org, repo, user)
+	if err != nil {
+		m.log.Error(err, "Failed to check if user is trusted")
+		return err
+	}
+	if !trusted {
+		m.log.Info("User is not trusted")
+		return nil
+	}
+
+	// Extract the new title and issue number
+	var newTitle string
+	var issueNumber float64
+	if context.Event != nil {
+		if comment, ok := context.Event["comment"].(map[string]any); ok {
+			// Extract the comment body
 			if body, ok := comment["body"].(string); ok {
 				// Make sure they are requesting a re-title
 				if !retitleRe.MatchString(body) {
@@ -83,8 +120,9 @@ func (m command) run(c *cli.Context) error {
 				newTitle = getNewTitle(body)
 			}
 		}
+		// get the issue number
 		if issue, ok := context.Event["issue"].(map[string]any); ok {
-			if num, ok := issue["number"].(int); ok {
+			if num, ok := issue["number"].(float64); ok {
 				issueNumber = num
 			}
 		}
@@ -92,15 +130,11 @@ func (m command) run(c *cli.Context) error {
 	m.log.Info("New title", "title", newTitle)
 	m.log.Info("Issue number", "number", issueNumber)
 
-	org, repo := context.Repo()
-	ghToken := action.Getenv("GITHUB_TOKEN")
-	gh := github.NewClient(nil).WithAuthToken(ghToken)
-
 	// Update the title
 	req := &github.IssueRequest{
 		Title: &newTitle,
 	}
-	_, _, err = gh.Issues.Edit(c.Context, org, repo, issueNumber, req)
+	_, _, err = gh.Issues.Edit(c.Context, org, repo, int(issueNumber), req)
 	if err != nil {
 		m.log.Error(err, "Failed to update issue")
 		return err
@@ -115,4 +149,43 @@ func getNewTitle(body string) string {
 		return ""
 	}
 	return strings.TrimSpace(matches[1])
+}
+
+type owners struct {
+	Reviewers []string `json:"reviewers"`
+	Approvers []string `json:"approvers"`
+}
+
+func isTrusted(ctx context.Context, gh *github.Client, org, repo, user string) (bool, error) {
+	// Read owners file
+	ownersFile, _, _, err := gh.Repositories.GetContents(ctx, org, repo, "OWNERS", nil)
+	if err != nil {
+		return false, err
+	}
+
+	// Decode the file
+	ownersData, err := ownersFile.GetContent()
+	if err != nil {
+		return false, err
+	}
+
+	// Check if the user is in the file
+	var owners owners
+	// Unmarshal the YAML string into the struct
+	err = yaml.Unmarshal([]byte(ownersData), &owners)
+	if err != nil {
+		panic(err)
+	}
+	for _, reviewer := range owners.Reviewers {
+		if reviewer == user {
+			return true, nil
+		}
+	}
+	for _, approver := range owners.Approvers {
+		if approver == user {
+			return true, nil
+		}
+	}
+
+	return false, nil
 }
